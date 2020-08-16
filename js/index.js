957e90c2d5bff51ae2579b559af8916049a7ab36
jaesonwatts <jaeson.watts@gmail.com>
2020-08-16 17:52:22 -0600

    post fetch working

--- js/index.js
+++ js/index.js
@@ -1,25 +1,72 @@
-const memberContainer = document.getElementById('member-container')
-const membersPath = "http://localhost:3000/api/v1/members"
+const personaContainer = document.getElementById('persona-container')
+const personasPath = "http://localhost:3000/api/v1/personas"
 
 document.addEventListener('DOMContentLoaded', () => {
-  getMembers()
+  getPersonas()
+
+  const createPersonasForm = document.querySelector("#persona-form-container")
+
+  createPersonasForm.addEventListener("submit", (e) => createFormHandler(e))
 })
 
-function getMembers() {
-  fetch(membersPath)
+function getPersonas() {
+  fetch(personasPath)
   .then(response => response.json())
-  .then(members => {
-    members.data.forEach(member => {
-      const membersMarkup = `
-        <div data-id=${member.attributes.id}>
-          <h3><span>Member Name: ${member.attributes.name}</span></h3>
-          <h4><span>Email: ${member.attributes.email}</span></h4>
-          <h4><span>Admin Level: ${member.attributes.admin_level}</span></h4>
-          <button data-id=${members.id}>edit</button>
+  .then(personas => {
+    personas.data.forEach(persona => {
+      const personasMarkup = `
+        <div data-id=${persona.attributes.id}>
+          <h3><span>Persona Name: ${persona.attributes.first_name} ${persona.attributes.last_name}</span></h3>
+          <h4><span>Date of Birth: ${persona.attributes.dob}</span></h4>
+          <h4><span>Sex: ${persona.attributes.sex}</span></h4>
+          <h4><span>Race: ${persona.attributes.race}</span></h4>
+          <h4><span>Faction Name: ${persona.attributes.faction.name}</span></h4>
+          <h4><span>Belongs to Member: ${persona.attributes.member.name}</span></h4>
+          <button data-id=${personas.id}>edit</button>
         </div>
         <br><br>
       `;
-      document.querySelector('#members-container').innerHTML += membersMarkup
+      document.querySelector('#personas-container').innerHTML += personasMarkup
     })
+  })
+}
+
+function createFormHandler(e) {
+  e.preventDefault()
+  const firstNameInput = document.querySelector('#input-first-name').value
+  const lastNameInput = document.querySelector('#input-last-name').value
+  const dobInput = document.querySelector('#input-dob').value
+  const sexInput = document.querySelector('#input-sex').value
+  const raceInput = document.querySelector('#input-race').value
+  const factionInput = document.querySelector('#select-factions').value
+  const memberInput = document.querySelector('#select-members').value
+  postFetch(firstNameInput, lastNameInput, dobInput, sexInput, raceInput, factionInput, memberInput) 
+}
+
+
+function postFetch(first_name, last_name, dob, sex, race, faction_id, member_id) {
+ 
+  const bodyData = {first_name, last_name, dob, sex, race, faction_id, member_id}
+
+  fetch(personasPath, {
+    method: 'POST',
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(bodyData)
+  })
+  .then(response => response.json())
+  .then(persona => {
+  
+    const personaData = persona.data.attributes
+    const personaMarkup = `
+      <div data-id=${persona.id}>
+        <h3><span>Persona Name: ${personaData.first_name} ${personaData.last_name}</span></h3>
+        <h4><span>Date of Birth: ${personaData.dob}</span></h4>
+        <h4><span>Sex: ${personaData.sex}</span></h4>
+        <h4><span>Race: ${personaData.race}</span></h4>
+        <h4><span>Faction Name: ${personaData.faction.name}</span></h4>
+        <h4><span>Belongs to Member: ${personaData.member.name}</span></h4>
+        <button data-id=${personaData.id}>Edit</button>
+      </div>
+    `
   })
 }
